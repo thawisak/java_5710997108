63dc1db726334b33d8fc0c002c9265a7e28c4376
thawisak <kunggong87@gmail.com>
2017-01-31 15:36:23 +0700

    งานวันที่30มกราคม2560

---
+++ JavaApplication2/src/javaapplication2/GenerateBill.java
@@ -0,0 +1,11 @@
+
+package javaapplication2;
+
+ class GenerateBill {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ JavaApplication2/src/javaapplication2/Plan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Kung
+ */
+class Plan {
+    protected double rate;
+    
+    public void getRate(){
+        
+    }
+    
+    public void CalculateBill(int units){
+        
+    }
+}
